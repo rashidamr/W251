db0fe28565681b807712d003ad2258a27e882dad
rashidamr <mail@arashid.net>
2017-06-09 01:24:21 +0200

    l

--- download_1.sh
+++ download_1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -P cd /gpfs/gpfsfpo/gpfs1 http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-1.csv.zip
+wget -P /gpfs/gpfsfpo/gpfs1 http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-1.csv.zip
