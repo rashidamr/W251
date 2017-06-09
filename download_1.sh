637ffeb517e3c5db9cadedfe5d8acd1dd48d4f09
rashidamr <mail@arashid.net>
2017-06-09 11:30:17 +0200

    l

--- download_1.sh
+++ download_1.sh
@@ -8,6 +8,6 @@ do
    echo "Downloading file id ${i}"
    wget -P /gpfs/gpfsfpo/gpfs1 http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-$i.csv.zip
    echo "Unzipping file id ${i}"
-   unzip 20090715-$i.csv.zip -d /gpfs/gpfsfpo/gpfs1
+   unzip googlebooks-eng-all-2gram-20090715-$i.csv.zip -d /gpfs/gpfsfpo/gpfs1
    rm 20090715-$i.csv.zip
 done
